fd41d622b74e9109a28fb8d1793ba33c3b273fbc
orpd0523 <orpd0523@gmail.com>
2023-04-06 14:47:11 -0500

    creating start button event listener and start the count

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-let start = document.getElementsById("#")
+//referencing buttons into variables
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
+const timer = document.querySelector("#timer");
+
+//create global variable for the timer
+let count; //variable will hold setInterval function
+let i = 0; //variable starts at 0
+
+//create function for start variable
+//add eventlistener
+start.addEventListener("click", () => {
+  count = setInterval(() => {
+    i++; //adding 1 to i variable that starts at 0
+    timer.innerHTML = i; //writing JS into HTML to show in browser
+    console.log("I started counting"); //showing in console to check if it is working
+  }, 1000);
+});
+
+//create function for stop variable
+//add eventlistener
+
+//create function for reset variable
+//add eventlistener
